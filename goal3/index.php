d656baf873e9289505d4bc3481473ec77a763d07
menshey <33385601+menshey@users.noreply.github.com>
2017-11-07 00:41:27 -0800

    Add files via upload

---
+++ goal3/index.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+   <link rel="stylesheet" href="style.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+   <title>My Appointment</title>
+
+
+   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+   <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- CSS files-->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css" integrity="sha384-rHyoN1iRsVXV4nD0JutlnGaslCJuC7uwjduW9SVrLvRYooPp2bWYgmgJQIXwl/Sp" crossorigin="anonymous">
+   </head>
+  <body>
+    <!-- page content -->
+
+    <div id='cssmenu'>
+  </br>
+      <h><font size= "25" font face= "Source Sans Pro" color="white" >  My Appointment </font></h>
+      <div align="right" class="icon-bar">
+      <a class="active" href="index.php"><i class="fa fa-home" style="font-size:40px;color:white"></i></a> 
+      <a class="#" href="calendar.php"><i class="fa fa-calendar"style="font-size:40px;color:white"></i></a>
+      <i class="material-icons" style="font-size:40px;color:white">textsms</i>
+    </div>
+  </div>
+
+<table class="table table-bordered table-striped table-hover table-condensed table-responsive">
+    </br>
+  <thead>
+    <tr>
+      <th>
+        Full Name
+      </th>
+      <th>
+        WebSite
+      </th>
+      <th>
+        Contact No
+      </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <td>
+        John M
+      </td>
+      <td>
+        http://john-m.com
+      </td>
+      <td>
+        9876543210
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Ariana Smith
+      </td>
+      <td>
+        https://araiana-smith.com
+      </td>
+      <td>
+        1234567890
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Silver Bourne
+      </td>
+      <td>
+        https://silver-bourne.com
+      </td>
+      <td>
+        988889888
+      </td>
+    </tr>
+    <tr>
+      <td>
+        Rahul Ray
+      </td>
+      <td>
+        https://rahul-ray.com
+      </td>
+      <td>
+        9797979797
+      </td>
+    </tr>
+  </tbody>
+</table>
+    <!--JS Files-->
+<script  src="https://code.jquery.com/jquery-3.2.1.min.js" integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="   crossorigin="anonymous"></script>
+<script src="bootstable.min.js"></script>
+  </body>
+</html>
